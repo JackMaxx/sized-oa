048b4205d0718fa16aa34ae49666be2ce7e48c8b
zhangwt <zhangwt@suninfo.com>
2022-09-01 20:38:54 +0800

    添加修改密码功能

--- src/page/PersonnelManagement.vue
+++ src/page/PersonnelManagement.vue
@@ -13,13 +13,16 @@
       <el-table-column prop="realName" label="人员姓名"></el-table-column>
       <el-table-column prop="entryTime" label="入职时间"></el-table-column>
       <el-table-column prop="loginName" label="账号"></el-table-column>
-      <el-table-column label="操作" width="120">
+      <el-table-column label="操作" width="160">
         <template slot-scope="scope">
           <el-button @click="edit(scope.row)" type="text" size="small">
             编辑
           </el-button>
           <el-button @click="transfer(scope.row)" type="text" size="small">
             转出
+          </el-button>
+          <el-button @click="editPassword(scope.row)" type="text" size="small">
+            修改密码
           </el-button>
         </template>
       </el-table-column>
@@ -37,12 +40,14 @@
         <el-button type="primary" @click="confirm">确 定</el-button>
       </span>
     </el-dialog>
-    <el-dialog title="转出" :visible.sync="transferOutVisible" width="600px" :close-on-click-modal="false" :close-on-press-escape="false">
+    <el-dialog title="转出" :visible.sync="transferOutVisible" width="600px" :close-on-click-modal="false"
+      :close-on-press-escape="false">
       <el-form :model="transForm" :rules="trans" ref="transForm" label-width="100px" class="demo-ruleForm"
         label-position="right">
         <el-form-item label="人员姓名：" prop="userUuid" v-if="transferOutVisible">
           <el-select v-model="transForm.userUuid" placeholder="请选择人员">
-            <el-option v-for="(item, index) in personnelNameList" :label="item.realName" :value="item.uuid" :key="index">
+            <el-option v-for="(item, index) in personnelNameList" :label="item.realName" :value="item.uuid"
+              :key="index">
             </el-option>
           </el-select>
         </el-form-item>
@@ -52,19 +57,50 @@
         <el-button type="primary" @click="transferOut">确 定</el-button>
       </span>
     </el-dialog>
+    <el-dialog title="修改密码" :visible.sync="changePasswordVisible" width="600px" class="dialog-wrap"
+      :close-on-click-modal="false" :close-on-press-escape="false">
+      <el-form :model="map" :rules="rules" ref="updatePwd" label-width="100px" class="demo-ruleForm">
+        <el-form-item label="当前密码：" prop="oldPwd">
+          <el-input type="password" v-model="map.oldPwd" style="width: 414px" placeholder="请输入"></el-input>
+        </el-form-item>
+        <el-form-item label="新密码：" prop="pwd">
+          <el-input type="password" v-model="map.pwd" style="width: 414px" placeholder="请输入"></el-input>
+        </el-form-item>
+        <el-form-item label="确认密码：" prop="cfmNewPassword">
+          <el-input type="password" v-model="map.cfmNewPassword" style="width: 414px" placeholder="请输入"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="changePasswordVisible = false">取 消</el-button>
+        <el-button type="primary" @click="submitForm">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
 <script type="text/babel">
 import addPersonnel from './Dialog/AddPersonnel.vue'
 import { timeStamp } from '../util/date'
+import { base64 } from '../util/base64'
 import { cloneDeep } from 'lodash'
 export default {
   components: {
     addPersonnel
   },
   data () {
+    const checkSame = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请确认密码！'))
+      } else {
+        if (value !== this.map.pwd) {
+          callback(new Error('两次密码不一致！'))
+        } else {
+          callback()
+        }
+      }
+    }
     return {
+      changePasswordVisible: false,
       dialogVisible: false,
       transferOutVisible: false,
       title: '',
@@ -83,7 +119,22 @@ export default {
         ]
       },
       rowData: null,
-      targetUuid: ''
+      targetUuid: '',
+      map: {
+        oldPwd: '',
+        pwd: ''
+      },
+      rules: {
+        oldPwd: [
+          { required: true, message: '请输入当前密码', trigger: 'blur' }
+        ],
+        pwd: [
+          { required: true, message: '请输入新密码', trigger: 'blur' }
+        ],
+        cfmNewPassword: [
+          { required: true, validator: checkSame, trigger: 'blur' }
+        ]
+      }
     }
   },
   mounted () {
@@ -121,6 +172,36 @@ export default {
       this.title = '编辑'
       this.dialogVisible = true
       this.rowData = row
+    },
+    // 修改密码
+    editPassword () {
+      this.changePasswordVisible = true
+    },
+    // 保存修改密码
+    submitForm () {
+      this.$refs.updatePwd.validate((valid) => {
+        if (valid) {
+          // eslint-disable-next-line new-cap
+          const encrypt = new base64()
+          const send = {
+            oldPwd: encrypt.encode(this.map.oldPwd),
+            pwd: encrypt.encode(this.map.pwd)
+          }
+          this.$axios.post('/suninfo-userasset/userinfo/updateUserPwd', send).then(({ data }) => {
+            if (data.success) {
+              this.$message({
+                type: 'success',
+                message: data.msg
+              })
+            } else {
+              this.$message({
+                type: 'error',
+                message: data.msg
+              })
+            }
+          })
+        }
+      })
     },
     transfer (row) {
       this.transferOutVisible = true
